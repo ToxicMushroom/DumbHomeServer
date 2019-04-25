17a531fb05658371311109e888c98743f403ecb7
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-04-25 22:13:17 +0200

    Added some helper methods to keep Database class kinda clean

--- src/main/java/me/melijn/dhs/Helpers.java
+++ src/main/java/me/melijn/dhs/Helpers.java
@@ -1,14 +1,31 @@
 package me.melijn.dhs;
 
+import me.melijn.dhs.components.SwitchComponent;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Helpers {
 
 
-    public JSONArray getPresetList() {
-        JSONArray jsonArray = new JSONArray();
+    private final Database database;
+
+    public Helpers(Database database) {
+        this.database = database;
+    }
 
 
-        return jsonArray;
+    public JSONObject getPresets(String user) {
+        JSONObject jsonObject = new JSONObject();
+        JSONArray switchArray = new JSONArray();
+        for (SwitchComponent switchComponent : database.getSwitchPresets(user)) {
+            switchArray.put(new JSONObject()
+                    .put("id", switchComponent.getId())
+                    .put("name", switchComponent.getName())
+                    .put("location", switchComponent.getLocation()));
+
+        }
+        jsonObject.put("switches", switchArray);
+
+        return jsonObject;
     }
 }
