54f0fdc46432170143b80214ade8b8265fb2f6c0
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-04-28 10:24:12 +0200

    Added methods and redone some tables

--- src/main/java/me/melijn/dhs/storage/Database.java
+++ src/main/java/me/melijn/dhs/storage/Database.java
@@ -1,8 +1,10 @@
-package me.melijn.dhs;
+package me.melijn.dhs.storage;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import me.melijn.dhs.components.Location;
 import me.melijn.dhs.components.SwitchComponent;
+import org.jooby.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,16 +13,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class Database {
 
     private final HikariDataSource ds;
     private final Logger logger = LoggerFactory.getLogger(Database.class.getName());
-    public Set<SwitchComponent> switchComponentSet = new HashSet<>();
 
 
     public Database(String host, String port, String user, String password, String db) {
@@ -54,12 +55,15 @@ public class Database {
         //Tables
         executeUpdate("CREATE TABLE IF NOT EXISTS users(username varchar(32), token varchar(64), PRIMARY KEY (username));");
         executeUpdate("CREATE TABLE IF NOT EXISTS log(username varchar(32), action varchar(512), moment bigint);");
-        executeUpdate("CREATE TABLE IF NOT EXISTS switches(id int, on_code int, off_code int, last_state boolean, PRIMARY KEY (id));");
-        executeUpdate("CREATE TABLE IF NOT EXISTS remote_btn_index(id varchar(64), codeSequence varchar(1024), PRIMARY KEY (id));");
+        executeUpdate("CREATE TABLE IF NOT EXISTS remote_buttons(id varchar(64), codeSequence varchar(1024), PRIMARY KEY (id));");
 
-        executeUpdate("CREATE TABLE IF NOT EXISTS button_presets(username varchar(32), name varchar(32), location varchar(64), code int);");
-        executeUpdate("CREATE TABLE IF NOT EXISTS switch_presets(username varchar(32), name varchar(32), location varchar(64), id int);");
-        executeUpdate("CREATE TABLE IF NOT EXISTS view_presets(username varchar(32), name varchar(32), location varchar(64), url varchar(128));");
+        executeUpdate("CREATE TABLE IF NOT EXISTS views(id int, name varchar(32), url varchar(128), location varchar(64), PRIMARY KEY (id));");
+        executeUpdate("CREATE TABLE IF NOT EXISTS buttons(id int, name varchar(32), url varchar(128), location varchar(64), PRIMARY KEY (id));");
+        executeUpdate("CREATE TABLE IF NOT EXISTS switches(id int, name varchar(32), on_code int, off_code int, location varchar(64), last_state boolean, PRIMARY KEY (id));");
+
+        executeUpdate("CREATE TABLE IF NOT EXISTS button_presets(username varchar(32), name varchar(32), id int);");
+        executeUpdate("CREATE TABLE IF NOT EXISTS switch_presets(username varchar(32), id int, name varchar(32), on_code int, off_code int, location varchar(64), last_state boolean);");
+        executeUpdate("CREATE TABLE IF NOT EXISTS view_presets(username varchar(32), name varchar(32), id int);");
 
 
         logger.info("[MySQL] init loaded");
@@ -96,11 +100,11 @@ public class Database {
         }
     }
 
-    public List<SwitchComponent> getSwitchPresets(String user) {
+    public List<SwitchComponent> getSwitchPresets(String username) {
         List<SwitchComponent> switchComponents = new ArrayList<>();
         try (Connection con = ds.getConnection();
              PreparedStatement statement = con.prepareStatement("SELECT * FROM switch_presets WHERE username = ?")) {
-            statement.setString(1, user);
+            statement.setString(1, username);
             try (ResultSet rs = statement.executeQuery()) {
                 while (rs.next()) {
                     //TODO use cache instead of query
@@ -112,5 +116,51 @@ public class Database {
         }
 
         return switchComponents;
+    }
+
+    public List<SwitchComponent> getSwitchList() {
+        List<SwitchComponent> switchComponents = new ArrayList<>();
+        try (Connection con = ds.getConnection();
+             PreparedStatement statement = con.prepareStatement("SELECT * FROM switches")) {
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    switchComponents.add(new SwitchComponent(
+                            rs.getString("name"),
+                            Location.valueOf(rs.getString("location")),
+                            rs.getInt("id"),
+                            rs.getBoolean("last_state"),
+                            rs.getInt("on_code"),
+                            rs.getInt("off_code")
+                    ));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return switchComponents;
+    }
+
+
+    public Map<String, String> getUserTokens() {
+        Map<String, String> userTokens = new HashMap<>();
+        try (Connection con = ds.getConnection();
+             PreparedStatement statement = con.prepareStatement("SELECT * FROM users")) {
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                userTokens.put(rs.getString("name"), rs.getString("token"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userTokens;
+    }
+
+    public void log(String user, Request req) {
+        executeUpdate("INSERT INTO log (username, action, moment) VALUES (?, ?, ?)", user, req.ip() + "/" + req.method() + " - " + req.path(), System.currentTimeMillis());
+    }
+
+    public void updateSwitchState(int id, boolean state) {
+        executeUpdate("UPDATE switches SET last_state= ? WHERE id= ?", state, id);
     }
 }
