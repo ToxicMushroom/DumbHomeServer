887e0816e703c8134ab8610c0c09cbee56a9671e
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-04-28 20:09:17 +0200

    Fixed rf code sending

--- src/main/java/me/melijn/dhs/utils/Helpers.java
+++ src/main/java/me/melijn/dhs/utils/Helpers.java
@@ -1,20 +1,26 @@
 package me.melijn.dhs.utils;
 
+import com.pi4j.io.gpio.RaspiPin;
 import me.melijn.dhs.components.SwitchComponent;
+import me.melijn.dhs.rcswitch.Protocol;
+import me.melijn.dhs.rcswitch.RCSwitch;
 import me.melijn.dhs.storage.CacheManager;
 import me.melijn.dhs.storage.Database;
 import org.jooby.Request;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class Helpers {
 
-
+    private final RCSwitch rcSwitch = new RCSwitch(RaspiPin.GPIO_00, Protocol.PROTOCOL_433);
     private final Database database;
     private final CacheManager cacheManager;
     private final TaskManager taskManager;
+    private final Logger logger = LoggerFactory.getLogger(Helpers.class.getName());
 
 
     public Helpers(Database database, CacheManager cacheManager, TaskManager taskManager) {
@@ -50,15 +56,19 @@ public class Helpers {
         SwitchComponent switchComponent = cacheManager.getSwitchComponentById(id);
         if (switchComponent == null) return null;
 
+        taskManager.async(() -> {
+            int decimal = state ? switchComponent.getOnCode() : switchComponent.getOffCode();
+            String code = Integer.toBinaryString(decimal);
+            code = new String(new char[(24 - code.length())]).replace("\0", "0") + code;
+            logger.info("rf code sent: " + decimal + "/" + code);
+            rcSwitch.send(code); //GPIO is GPIO17 but undercover, pls save me from this suffering
+
+            database.updateSwitchState(id, state);
+        });
+
         cacheManager.switchComponentList.remove(switchComponent);
         switchComponent.setOn(state);
         cacheManager.switchComponentList.add(switchComponent);
-
-        taskManager.async(() -> {
-            String code = Integer.toBinaryString(state ? switchComponent.getOnCode() : switchComponent.getOffCode());
-            //new RCSwitch(RaspiPin.GPIO_07, Protocol.PROTOCOL_433).send(code);
-            database.updateSwitchState(id, state);
-        });
 
         return switchComponent;
     }
